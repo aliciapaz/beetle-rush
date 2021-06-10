6670edf4ed00a90f6a6c2edf7e575fe739ce67cf
aliciapaz <aliciapazrojas@gmail.com>
2021-06-10 13:09:14 -0400

    Import scenes and config files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('im ready to code a game!!!')
+import 'phaser';
+import config from './config/config';
+import GameScene from './scenes/game';
+import BootScene from './scenes/boot';
+import PreloaderScene from './scenes/preloader';
+import TitleScene from './scenes/title';
+import OptionsScene from './scenes/options';
+import CreditsScene from './scenes/credits';
+ 
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Boot');
+  }
+}
+ 
+window.game = new Game();

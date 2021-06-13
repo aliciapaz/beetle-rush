fee7e4b11dbf81689af2f4118e53bd363f0d3590
aliciapaz <aliciapazrojas@gmail.com>
2021-06-13 17:10:34 -0400

    Add global state

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import PreloaderScene from './scenes/preloader';
 import TitleScene from './scenes/title';
 import OptionsScene from './scenes/options';
 import CreditsScene from './scenes/credits';
+import Model from './model'
  
 class Game extends Phaser.Game {
   constructor () {
     super(config);
+    const model = new Model()
+    this.globals = { model }
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);

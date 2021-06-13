669a4e8d2d627fb7251618a4e5c2b7239a6a27da
aliciapaz <aliciapazrojas@gmail.com>
2021-06-13 17:11:48 -0400

    Allow enable and disable music

--- src/scenes/options.js
+++ src/scenes/options.js
@@ -5,11 +5,8 @@ export default class OptionsScene extends Phaser.Scene {
     super("Options");
   }
 
-  preload() {}
-
   create() {
-    this.musicOn = true;
-    this.soundOn = true;
+    this.model = this.sys.game.globals.model;
 
     this.text = this.add.text(300, 100, "Options", { fontSize: 40 });
     this.musicButton = this.add.image(200, 200, "checkedBox");
@@ -24,7 +21,7 @@ export default class OptionsScene extends Phaser.Scene {
     this.musicButton.on(
       "pointerdown",
       function () {
-        this.musicOn = !this.musicOn;
+        this.model.musicOn = !this.model.musicOn;
         this.updateAudio();
       }.bind(this)
     );
@@ -32,7 +29,7 @@ export default class OptionsScene extends Phaser.Scene {
     this.soundButton.on(
       "pointerdown",
       function () {
-        this.soundOn = !this.soundOn;
+        this.model.soundOn = !this.model.soundOn;
         this.updateAudio();
       }.bind(this)
     );
@@ -59,16 +56,17 @@ export default class OptionsScene extends Phaser.Scene {
 
     this.menuButton.on("pointerout", function (event, gameObjects) {
       this.setTexture("blueButton1");
-    })
+    });
   }
+
   updateAudio() {
-    if (this.musicOn === false) {
+    if (this.model.musicOn === false) {
       this.musicButton.setTexture("box");
     } else {
       this.musicButton.setTexture("checkedBox");
     }
 
-    if (this.soundOn === false) {
+    if (this.model.soundOn === false) {
       this.soundButton.setTexture("box");
     } else {
       this.soundButton.setTexture("checkedBox");

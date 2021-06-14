08037c8ca1bfeec98e1c2244b1275fb62993faf5
aliciapaz <aliciapazrojas@gmail.com>
2021-06-14 01:06:29 -0400

    Add scrolling ground

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -14,16 +14,18 @@ export default class GameScene extends Phaser.Scene {
   create() {
     // Make Background, Ground and score dashboard
     this.background = this.add.tileSprite(400,300,800,1200,"background").setScrollFactor(0,1)
+    this.ground = this.add.tileSprite(400,580,800,70, 'platform').setScrollFactor(0,1)
+
     this.scoreText = this.add.text(16, 16, "score: 0", {
       fontSize: "32px",
       fill: "#000",
     });
-    
-    //this.platforms = this.physics.add.staticGroup();
-    // this.platforms.create(350, 522, "platform").setScale(1);
+
   }
 
   update () {
     this.background.tilePositionX += 1;
+    this.ground.tilePositionX += 2;
+
  }
 }
